f6c8972c2a99e8d591337e1579dcb5882e326b38
hushilin <hushilin@sugo.io>
2021-02-19 22:06:30 +0800

    提交日志

--- packages/vue/src/main.js
+++ packages/vue/src/main.js
@@ -37,6 +37,7 @@ function render(props = {}) {
 if (window.__POWERED_BY_QIANKUN__) {
   __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__;
 }
+
 // 独立运行
 if (!window.__POWERED_BY_QIANKUN__) {
   render();
