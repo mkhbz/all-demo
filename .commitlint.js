839ed06e170b3e098d3852a71c1c8fcdcdbcac89
hushilin <hushilin@sugo.io>
2021-02-19 22:37:02 +0800

    修改module.exports = {

--- .commitlint.js
+++ .commitlint.js
@@ -1,22 +1,17 @@
-// commit 日志规范
 module.exports = {
-  extends: ["@commitlint/config-conventional"],
+  extends: [
+    "@commitlint/config-conventional"
+  ],
   rules: {
-    "type-enum": [
-      0,
-      "always",
-      [
-        "build", // 发布版本
-        "chore", // 改变构建流程、或者增加依赖库、工具等
-        "ci", // 持续集成修改
-        "docs", // 文档修改
-        "feat", // 新增功能
-        "fix",  // 修复缺陷
-        "perf", // 优化相关，比如提升性能、体验
-        "refactor", // 代码重构
-        "revert", // 回退版本
-        "style", // 样式修改
-        "test", // 测试用例修改
-      ],
-    ],
-  },
+    'type-enum': [2, 'always', [
+      'upd', 'feat', 'fix', 'refactor', 'docs', 'chore', 'style', 'revert'
+    ]],
+    'type-case': [0],
+    'type-empty': [0],
+    'scope-empty': [0],
+    'scope-case': [0],
+    'subject-full-stop': [0, 'never'],
+    'subject-case': [0, 'never'],
+    'header-max-length': [0, 'always', 72]
+  }
+};
