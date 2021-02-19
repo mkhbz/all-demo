baef75bf68bda28c501807606ac3e05379269c3d
hushilin <hushilin@sugo.io>
2021-02-19 21:42:18 +0800

    feat: 添加husky

--- packages/vue/src/main.js
+++ packages/vue/src/main.js
@@ -37,7 +37,6 @@ function render(props = {}) {
 if (window.__POWERED_BY_QIANKUN__) {
   __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__;
 }
-
 // 独立运行
 if (!window.__POWERED_BY_QIANKUN__) {
   render();
