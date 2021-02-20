da790575052de8ff959d2b44145d780385d755b9
hushilin <hushilin@sugo.io>
2021-02-20 16:17:15 +0800

    123

--- script/commit.js
+++ script/commit.js
@@ -1,5 +1,26 @@
 console.log('提交了代码')
+// fs模块读取当前的commit日志
+// .git/COMMIT_EDITMSG
+const msg = require('fs').readFileSync('.git/COMMIT_EDITMSG', 'utf-8').trim()
+console.log('提交的日志是')
+console.log(msg)
+const start = [
+    'feat :',
+    'fix :',
+    'docs :',
+    'refactor :',
+    'chore :',
+    'style :',
+    'revert :',
+    '`Merge branch ',
 
-console.log(process.argv)
-//阻止进程
-process.exit()
+]
+function isBelong(arg1) {
+    return msg.startsWith(arg1)
+}
+if (!start.filter(isBelong)) {
+    console.log('找不到')
+    //阻止进程
+    process.exit(1)
+}
+console.log('提交成功了')
